289d7b87ed23c1afc66b3aea16f4ecf7f787bc6e
Leo <leoquan09@gmail.com>
2025-12-02 17:02:15 -0800

    I FINALLY FINISHED

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ let currentNum = '';
 let currentOperator = '';
 let pastOperand = '';
 
+let displayText = document.querySelector('.display');
+
 function appendNum(num) {
-	currentNum += num;
-	document.querySelector('.display').textContent = currentNum;
+    // if (num = '.' && currentNum.includes('.')) {
+    //     num = '';
+    // }
+    currentNum += num;
+	displayText.textContent = currentNum;
     console.log(currentNum);
 }
 
@@ -12,7 +17,34 @@ function clear() {
     currentNum = '';
     currentOperator = '';
     pastOperand = '';
-    document.querySelector('.display').textContent = '000';
+    displayText.textContent = '0';
+}
+
+function calculate() {
+    const current = parseFloat(currentNum);
+    const past = parseFloat(pastOperand);
+    let result = 0;
+
+    switch (currentOperator) {
+        case '+':
+            result = past + current;
+            break;
+        case '-':
+            result = past - current;
+            break;
+        case '÷':
+            result = past / current;
+            break;
+        case 'x':
+            result = past * current;
+            break;
+    }
+
+    displayText.textContent = result;
+
+    currentNum = result.toString();
+    pastOperand = '';
+    currentOperator = '';
 }
 
 let nums = document.querySelectorAll('.nums');
@@ -24,3 +56,20 @@ nums.forEach(num => {
 
 let clearBtn = document.querySelector('.clear');
 clearBtn.addEventListener('click', clear);
+
+let operations = document.querySelectorAll('.operation');
+operations.forEach(operation => {
+    operation.addEventListener('click', () => {
+        if (pastOperand !== '' && currentNum !== '') {
+            calculate();
+        } 
+
+        displayText.textContent = operation.textContent;
+        currentOperator = operation.textContent;
+        pastOperand = currentNum;
+        currentNum = '';
+    })
+}) 
+
+let calcBtn = document.querySelector('.equal');
+calcBtn.addEventListener('click', calculate)
